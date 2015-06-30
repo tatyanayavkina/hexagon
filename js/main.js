f5843f733aa7ed302f2bf1964192bc54a020f410
tatyanayavkina <t.yavkina@mail.ru>
2015-06-30 15:42:48 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 main file of application
  */
 var app = new Application();
-var locale = Localization['ru'];
+var locale = Localization.ru;
 document.addEventListener('DOMContentLoaded', app.checkPageURI.bind(app), false);
 
